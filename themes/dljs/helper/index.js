d64e09abfaa344e834a025fbf3a61ec701d474aa
Yannick Assogba <yassogba@google.com>
2018-03-07 16:59:32 -0500

    basic render of api docs for current deeplearn.js docs

---
+++ themes/dljs/helper/index.js
@@ -0,0 +1,47 @@
+const MarkdownIt = require('markdown-it');
+const hljs = require('highlight.js');
+
+const md = new MarkdownIt({
+  highlight(str, lang) {
+    if (lang === 'js' && hljs.getLanguage(lang)) {
+      const highlighted = hljs.highlight(lang, str).value;
+      return '<pre class="hljs"><code class="hljs language-js">' + highlighted +
+          '</code></pre>\n';
+    }
+
+    return '';  // use external default escaping
+  }
+});
+
+
+module.exports = function(hexo) {
+  return {
+    toJson: function(obj) {
+      return JSON.stringify(obj);
+    },
+
+    isApiPage: function(path) {
+      return path.match(/^api/);
+    },
+
+    getApi: function(siteData, versionString) {
+      return siteData[`api/${versionString}/core-docs`];
+    },
+
+    markdown: function(attr) {
+      if (attr) {
+        return md.render(attr);
+      }
+    },
+
+    markdownInner: function(attr) {
+      if (attr) {
+        const asMd = md.render(attr.trim())
+                         .replace(/<p>/, '')
+                         .replace(/(<\/p>\s*)$/, '');
+
+        return asMd;
+      }
+    }
+  };
+};
