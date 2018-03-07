13dbf931519f7e4d035a863524efe32b15970d29
Yannick Assogba <yassogba@gmail.com>
2018-03-07 17:34:23 -0500

    Support programmatic creation of new versioned api pages (#3)
    * basic render of api docs for current deeplearn.js docs

---
+++ themes/dljs/source/js/api.js
@@ -0,0 +1,83 @@
+document.addEventListener('DOMContentLoaded', function(e) {
+  function executeCodeSnippet(consoleLogElement, codeSnippet) {
+    consoleLogElement.innerText = '';
+    var oldLog = console.log;
+    console.log = function(logStr) {
+      consoleLogElement.innerText += logStr + '\n';
+    };
+    var snippet = 'dl.tidy(function() {' + codeSnippet + '});';
+    eval(snippet);
+    console.log = oldLog;
+  };
+
+  function initCodeBlocks() {
+    // Find all the code blocks.
+    var jsBlocks = document.querySelectorAll('.language-js');
+    for (var i = 0; i < jsBlocks.length; i++) {
+      var block = jsBlocks[i];
+      var consoleElement = document.createElement('div');
+      consoleElement.className = 'snippet-console';
+      var consoleRunElement = document.createElement('button');
+      consoleRunElement.innerText = 'Run';
+      consoleRunElement.className = 'snippet-run-button';
+      var consoleLogElement = document.createElement('div');
+      consoleLogElement.className = 'snippet-console-log';
+      consoleElement.appendChild(consoleLogElement);
+      consoleElement.appendChild(consoleRunElement);
+      block.parentElement.insertAdjacentElement('afterend', consoleElement);
+      consoleRunElement.addEventListener('click', function() {
+        var consoleLogElement =
+            this.parentElement.querySelector('.snippet-console-log');
+        var snippetText = this.parentElement.previousElementSibling.innerText;
+        executeCodeSnippet(consoleLogElement, snippetText);
+      });
+    }
+  }
+  initCodeBlocks();
+
+
+  var isInViewport = function(elem) {
+    var bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 && bounding.left >= 0 &&
+        bounding.bottom <=
+            (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <=
+            (window.innerWidth || document.documentElement.clientWidth));
+  };
+  // Find the symbol closest to the top of the page in the reference
+  // section and highlight it in the TOC section.
+  var refSymbols = document.querySelectorAll('.symbol-name');
+  var refArea = document.querySelector('.reference');
+  var tocArea = document.querySelector('.toc');
+  var lastHighlightedTocElement;
+  function updateTocView() {
+    var top = refArea && refArea.scrollTop
+    var found;
+    var tocFound;
+    for (var i = 0; i < refSymbols.length; i++) {
+      var symbol = refSymbols[i];
+      var sTop = symbol.offsetTop;
+      if (sTop > top) {
+        found = symbol;
+        break;
+      }
+    }
+    if (found) {
+      tocFound =
+          tocArea.querySelectorAll(`[href="#${found.getAttribute('name')}"]`)
+      // The first subheading in a section shares an href with its parent.
+      tocFound = tocFound.length > 1 ? tocFound[1] : tocFound[0];
+      if (lastHighlightedTocElement) {
+        lastHighlightedTocElement.classList.remove('highlighted');
+      }
+      tocFound.classList.add('highlighted');
+      if (!isInViewport(tocFound)) {
+        tocFound.scrollIntoView(false);
+      }
+      lastHighlightedTocElement = tocFound;
+    }
+  }
+  refArea.addEventListener('scroll', updateTocView)
+  refArea.addEventListener('resize', updateTocView)
+});
