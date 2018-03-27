53d1daa075fe8e3bf46f4c99b4f7166f1c7301b7
Yannick Assogba <yassogba@google.com>
2018-03-27 11:12:47 -0400

    update build script to use package.json versions

--- build-scripts/build-api.js
+++ build-scripts/build-api.js
@@ -4,15 +4,14 @@ const path = require('path');
 const shell = require('shelljs');
 const commander = require('commander');
 const mkdirp = require('mkdirp');
+const cmp = require('semver-compare');
 
-// Get package.json of union package
-// const unionPackage =
-// JSON.parse(fs.readFileSync('node_modules/tfjs/package.json')); const
-// unionPackageVersion = unionPackage.version;
-const unionPackageVersion = '0.0.1';
+//
+// Helper functions
+//
 
+// This primarily exists to support github urls.
 function getVersion(depString) {
-  // This primarily exists to support github urls pre-release.
   return depString.match(/([\d.]+)$/)[0];
 }
 
@@ -28,66 +27,77 @@ function sh(cmd, errMsg) {
   bailOnFail(ret.code, errMsg);
 }
 
+//
+// Script Start
+//
+
 // Get command line params
-commander.option('--in [path]', 'main source entry')
-    .option('--package [path]', 'Package.json path')
-    .option('--src [path]', 'Path to src folder of repo')
-    .option('--repo [path]', 'Path to repo')
-    .option('--bundle  [path]', 'JS Bundle Path')
-    .option('--github [url]', 'Github repository URL')
-    .option('--out [path]', 'Output Path')
-    .option('--branch [branch name]', 'branch to build from')
-    .parse(process.argv);
+commander.option('--local', 'use a local build').parse(process.argv);
+
+// Get package.json of union package
+const unionPackage =
+    JSON.parse(fs.readFileSync('node_modules/@tensorflow/tfjs/package.json'));
+const unionPackageVersion =
+    commander.local ? 'local' : getVersion(unionPackage.version);
 
 // Get the version strings from the libray
-// const coreVersion = `v${getVersion(unionPackage.dependencies['tfjs-core'])}`;
-// const layersVersion =
-// `v${getVersion(unionPackage.dependencies['tfjs-layers'])}`;
-
-// const coreVersion = `v${'0.5.0'}`;
-const coreVersion = `master`;
-// const layersVersion = `v${'0.5.0'}`;
+const coreVersion =
+    `v${getVersion(unionPackage.dependencies['@tensorflow/tfjs-core'])}`;
+const layersVersion =
+    `v${getVersion(unionPackage.dependencies['@tensorflow/tfjs-layers'])}`;
 
 const docGenScript = 'build-scripts/doc-gen/make-api.ts';
 
-const pr = path.resolve;
-
-// TODO(nsthorat): Remove 'github' once deeplearn.js moves into TensorFlow.
 const libs = [
-  {repo: 'tfjs-core', github: 'https://github.com/PAIR-code/deeplearnjs'},
-  {repo: 'tfjs-layers', github: 'https://github.com/tensorflow/tfjs-layers'}
+  {
+    repo: 'tfjs-core',
+    github: 'https://github.com/tensorflow/tfjs-core/blob/master',
+    tag: coreVersion
+  },
+  {
+    repo: 'tfjs-layers',
+    github: 'https://github.com/tensorflow/tfjs-layers/blob/master',
+    tag: layersVersion
+  }
 ];
+
+// Store the paths to the generate docs for each component.
 const outputPaths = [];
+
 libs.forEach(lib => {
   const outputPath =
       path.resolve(`source/_data/api/${unionPackageVersion}/${lib.repo}.json`);
+
   const opts = {
-    input: (commander.in && pr(commander.in)) ||
-        path.resolve(`libs/${lib.repo}/src/index.ts`),
-    pkg: (commander.package && pr(commander.package)) ||
-        path.resolve(`libs/${lib.repo}/package.json`),
-    src: (commander.src && pr(commander.src)) ||
-        path.resolve(`libs/${lib.repo}/src/`),
-    repo: (commander.repo && pr(commander.repo)) ||
-        path.resolve(`libs/${lib.repo}/`),
+    input: path.resolve(`libs/${lib.repo}/src/index.ts`),
+    pkg: path.resolve(`libs/${lib.repo}/package.json`),
+    src: path.resolve(`libs/${lib.repo}/src/`),
+    repo: path.resolve(`libs/${lib.repo}/`),
     github: lib.github,
     out: outputPath,
   }
 
   const docGenCommand = `ts-node ${docGenScript} --in ${opts.input} --package ${
-      opts.pkg} --src ${opts.src} --bundle ${opts.bundle} --github ${
-      opts.github} --out ${opts.out} --repo ${opts.repo}`;
+      opts.pkg} --src ${opts.src} --github ${opts.github} --out ${
+      opts.out} --repo ${opts.repo}`;
+
+  // Prep the component. If "local" has been passed in then we do nothing
+  // to what is in libs. Else we want to check out a tag that correspond to
+  // to the version specified the component.
+  if (!commander.local) {
+    const checkoutCommand = `cd libs/${lib.repo} && git checkout ${lib.tag}`;
+  }
 
   console.log(`********* Generating docs for ${lib.repo} *********`);
-  // TODO: Swap this version with the version from the union package.
-  const version = 'master';
-  const coreBuild = shell.exec(
-      `cd libs/${lib.repo} && ` +
-      //`git checkout ${version} && ` +
-      `yarn && cd ../.. && ${docGenCommand}`);
-  bailOnFail(coreBuild.code, `Error building docs for ${lib.repo}`);
+
+  const buildCommand =
+      `cd libs/${lib.repo} && yarn && cd ../.. && ${docGenCommand}`;
+
+  sh(buildCommand, `Error building docs for ${lib.repo}`);
+
   outputPaths.push(outputPath);
 });
+
 
 console.log(`********* Merging docs *********`);
 
@@ -99,21 +109,22 @@ const skeletonPath = `source/_data/api/${unionPackageVersion}/skeleton.json`;
 // Copy master skeleton.json into version dir
 fs.copyFileSync('source/_data/api/skeleton.json', skeletonPath);
 
+// Bundle path will be included on every page of the site.
+const bundlePath = commander.bundle && path.resolve(commander.bundle) ||
+    path.resolve('node_modules/@tensorflow/tfjs/dist/tf.min.js');
+fs.copyFileSync(bundlePath, 'themes/dljs/source/js/vendor/tf.min.js');
 
-// TODO: Use the union package.
-const bundlePath = commander.bundle && pr(commander.bundle) ||
-    path.resolve('libs/tfjs-core/dist/deeplearn.js');
 // Merge the docs for each repo.
 const mergeResult = shell.exec(
     `ts-node ${mergeScript} --out ${mergeOutput} --skeleton ${skeletonPath} \
-    --bundlePath ${bundlePath} ${outputPaths.join(' ')}`);
+    ${outputPaths.join(' ')}`);
 bailOnFail(mergeResult.code, 'Error merging doc JSONs.');
 
 // Write docs manifest.
 const docsManifest = {
   tfjsVersion: unionPackageVersion,
   coreVersion,
-  // layersVersion,
+  layersVersion,
 };
 
 fs.writeFileSync(
@@ -129,21 +140,35 @@ const apiManifest = JSON.parse(fs.readFileSync(apiManifestPath));
 if (!apiManifest.versions.includes(unionPackageVersion)) {
   // Do not reformat the string below. It is yaml 'front-matter' format
   const pageTemplate = `---
-title: 0.0.1
-date: 2018-03-06 23:17:41
+title: ${unionPackageVersion}
 layout: api
 ---
 \n
 `;
 
   mkdirp.sync(`source/api/${unionPackageVersion}`);
-  const newApiPagePath = `source/api/${unionPackageVersion}/index.md`
+  const newApiPagePath = `source/api/${unionPackageVersion}/index.md`;
   fs.writeFileSync(newApiPagePath, pageTemplate);
 
-  apiManifest.versions.unshift(unionPackageVersion);
-  // TODO semver sort.
+  // Note we do not add 'local' to the api manifest, this prevents it from
+  // showing up in the dropdown menu.
+  if (unionPackageVersion !== 'local') {
+    apiManifest.versions.unshift(unionPackageVersion);
+    apiManifest.versions.sort(cmp).reverse();
+    fs.writeFileSync(apiManifestPath, JSON.stringify(apiManifest, null, 2));
 
-  fs.writeFileSync(apiManifestPath, JSON.stringify(apiManifest, null, 2));
+    // Override 'latest'. Do not reformat the string below
+    const latestPageTemplate = `---
+title: ${apiManifest.versions[0]}
+layout: api
+---
+\n
+`;
+
+    mkdirp.sync(`source/api/latest/`);
+    const newApiPagePath = 'source/api/latest/index.md'
+    fs.writeFileSync(newApiPagePath, latestPageTemplate);
+  }
 }
 
 // A this point a website build should be able to produce an api docs page
