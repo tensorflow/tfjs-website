805709e93eb6d37a55ab76ecc2edf8deaa0b267e
Yannick Assogba <yassogba@gmail.com>
2018-04-16 11:44:30 -0400

    Add a "try it live" in codepen link

--- themes/dljs/helper/index.js
+++ themes/dljs/helper/index.js
@@ -58,6 +58,32 @@ module.exports = function(hexo) {
 
     concat: function(a, b) {
       return a + b;
+    },
+
+    codepenHtml: function() {
+      return JSON.stringify(
+          `<h1>Try Tensorflow JS right in your browser!</h1>`);
+    },
+
+    codepenJs: function() {
+      return JSON.stringify(`// Define a model for linear regression.
+const model = tf.sequential();
+model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+
+// Prepare the model for training: Specify the loss and the optimizer.
+model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+
+// Generate some synthetic data for training.
+const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+
+// Train the model using the data.
+model.fit(xs, ys).then(() => {
+  // Use the model to do inference on a data point the model hasn't seen before:
+  // Open the browser devtools to see the output
+  model.predict(tf.tensor2d([5], [1, 1])).print();
+});
+      `);
     }
   };
 };
