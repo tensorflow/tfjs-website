42fc8d36e949bd1853ba823b7dccb496cba3b28f
Yannick Assogba <yassogba@google.com>
2019-02-26 14:53:00 -0500

    code review fixes

--- build-scripts/build-vis-api.ts
+++ build-scripts/build-vis-api.ts
@@ -1,3 +1,20 @@
+/**
+ * @license
+ * Copyright 2019 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
 import * as commander from 'commander';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -48,4 +65,4 @@ const templateFolder = `source/api_vis`;
 writeManifestAndTemplate(
     docsFolder, versionedDocsFolder, docsManifest, visVersion, templateFolder);
 
-// A this point a website build should be able to produce an api docs page
+// At this point a website build should be able to produce an api docs page
--- build-scripts/build_api_helpers.ts
+++ build-scripts/build_api_helpers.ts
@@ -1,3 +1,20 @@
+/**
+ * @license
+ * Copyright 2019 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
 import * as commander from 'commander';
 import * as fs from 'fs';
 import * as mkdirp from 'mkdirp';
@@ -40,7 +57,7 @@ export function generateDocs(libs: LibraryInfo[]): string[] {
       repo: path.resolve(`libs/${lib.repo}/`),
       github: lib.github,
       out: outputPath,
-    }
+    };
 
     const docGenCommand =
         `ts-node --project tsconfig.json ${docGenScript} ` +
@@ -70,7 +87,7 @@ export function generateDocs(libs: LibraryInfo[]): string[] {
 
     outputPaths.push(outputPath);
   });
-  return outputPaths
+  return outputPaths;
 }
 
 /**
