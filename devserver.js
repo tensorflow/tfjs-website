5e95b266e410be177c2d76ea5ca491a1d27dae67
Matthew Soulanille <msoulanille@google.com>
2021-02-22 07:51:42 -0800

    Replace http-server devserver with fastify (#409)

---
+++ devserver.js
@@ -0,0 +1,15 @@
+const fastify = require('fastify')();
+const path = require('path');
+const fastifyStatic = require('fastify-static');
+const PORT = 8080;
+
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, 'public'),
+});
+
+fastify.listen(PORT, (err, address) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`devserver started on ${address}`);
+});
