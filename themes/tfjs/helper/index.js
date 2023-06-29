21a7fa3b23929f76f25fdd10b29eacb7ea65ffae
Xu Xing <xing.xu@intel.com>
2023-06-29 10:28:26 +0800

    Use dropdown instead of select

--- themes/tfjs/helper/index.js
+++ themes/tfjs/helper/index.js
@@ -66,6 +66,26 @@ function isNonBrowserApiPage(path) {
       isTflitePage(path) || isTaskApiPage(path);
 }
 
+// WebGPU is supported since version 4.6.0, WebGL is 2.0.0, Wasm is 1.7.0.
+const firstWebgpuVersion = '4.6.0';
+const firstWebglVersion = '2.0.0';
+const firstWasmVersion = '1.7.0';
+function isVersionSupported(version, oldVersion) {
+  const versionArray = version.split('.');
+  const oldVersionArray = oldVersion.split('.');
+  for (var i = 0; i < versionArray.length; i++) {
+    const a = ~~versionArray[i];
+    const b = ~~oldVersionArray[i];
+    if (a > b) {
+      return true
+    };
+    if (a < b) {
+      return false;
+    }
+  }
+  return true;
+}
+
 module.exports = function(hexo) {
   return {
     toJson: function(obj) {
@@ -81,6 +101,20 @@ module.exports = function(hexo) {
     isApiNodePage: isApiNodePage,
 
     isNonBrowserApiPage: isNonBrowserApiPage,
+
+    isVersionSupported: isVersionSupported,
+
+    isWebGPUSupported: function(version) {
+      return isVersionSupported(version, firstWebgpuVersion);
+    },
+
+    isWebGLSupported: function(version) {
+      return isVersionSupported(version, firstWebglVersion);
+    },
+
+    isWasmSupported: function(version) {
+      return isVersionSupported(version, firstWasmVersion);
+    },
 
     getApi: function(siteData, versionString, path) {
       if (isApiVisPage(path)) {
